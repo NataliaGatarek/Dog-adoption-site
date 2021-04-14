4cd36233a3feff95e71ddafde381682a036e3119
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-14 17:01:11 +0200

    2.2 commit

--- new.js
+++ new.js
@@ -30,3 +30,20 @@ function showHide(event) {
         less.style.display = 'none';
     }
 }
+
+function showHideTwo(event) {
+  let moreText = document.getElementById("moreText");
+  let buttonID = event.target.id;
+  if (!moreText.classList.contains("show")) {
+    moreText.classList.add("show");
+    document.getElementById(buttonID).innerHTML = "Show Less";
+  } else {
+    moreText.classList.remove("show");
+    document.getElementById(buttonID).innerHTML = "Show More";
+  }
+}
+
+let button = document.getElementById("showMoreButton");
+button.addEventListener("click", function (event) {
+  showHideTwo(event);
+});
