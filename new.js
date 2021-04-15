f1aa1c554d054b53692f18b5effdbc2121336e39
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-15 15:42:32 +0200

    2.3 commit

--- new.js
+++ new.js
@@ -14,36 +14,79 @@ for (const thing of less) {
     })
 }
 
-function showHide(event) {
-    console.log(event.target.closest('div').id)
-    const num =event.target.closest('div').id
-    const dots = document.getElementById(num+"-dots");
-    const less = document.getElementById(num + "-less");
-    const more = document.getElementById(num + "-more");
-    if (dots.style.display !== 'none') {
+const showHide =(event) => {
+  console.log(event.target.closest('div').id)
+  const num = event.target.closest('div').id
+  const dots = document.getElementById(num + "-dots");
+  const less = document.getElementById(num + "-less");
+  const more = document.getElementById(num + "-more");
+   if (dots.style.display !== 'none') {
         dots.style.display = 'none';
-        more.style.display = 'inline';
-        less.style.display = 'inline';
-    } else {
+       more.style.display = 'inline';
+       less.style.display = 'inline';
+   } else {
         dots.style.display = 'inline';
-        more.style.display = 'none';
-        less.style.display = 'none';
+       more.style.display = 'none';
+       less.style.display = 'none';
     }
 }
+//function showHide(event) {
+  //  console.log(event.target.closest('div').id)
+   // const num =event.target.closest('div').id
+  //  const dots = document.getElementById(num+"-dots");
+  //  const less = document.getElementById(num + "-less");
+  //  const more = document.getElementById(num + "-more");
+  //  if (dots.style.display !== 'none') {
+  //      dots.style.display = 'none';
+  //      more.style.display = 'inline';
+   //     less.style.display = 'inline';
+   // } else {
+   //     dots.style.display = 'inline';
+   //     more.style.display = 'none';
+   //     less.style.display = 'none';
+   // }
+//}
 
-function showHideTwo(event) {
+//function showHideTwo(event) {
+ // let moreText = document.getElementById("moreText");
+ // let buttonID = event.target.id;
+ // if (!moreText.classList.contains("show")) {
+//    moreText.classList.add("show");
+ //   document.getElementById(buttonID).innerHTML = "Show Less";
+//  } else {
+ //   moreText.classList.remove("show");
+//    document.getElementById(buttonID).innerHTML = "Show More";
+ // }
+//}
+
+const showHideTwo = (event) => {
   let moreText = document.getElementById("moreText");
   let buttonID = event.target.id;
-  if (!moreText.classList.contains("show")) {
-    moreText.classList.add("show");
-    document.getElementById(buttonID).innerHTML = "Show Less";
-  } else {
-    moreText.classList.remove("show");
-    document.getElementById(buttonID).innerHTML = "Show More";
-  }
+ if (!moreText.classList.contains("show")) {
+   moreText.classList.add("show");
+   document.getElementById(buttonID).innerHTML = "Show Less";
+} else {
+   moreText.classList.remove("show");
+   document.getElementById(buttonID).innerHTML = "Show More";
+ }
 }
 
 let button = document.getElementById("showMoreButton");
-button.addEventListener("click", function (event) {
-  showHideTwo(event);
+if (button) {
+  button.addEventListener("click", function (event) {
+ showHideTwo(event);
+  });
+  }
+
+//} 
+//button.addEventListener("click", function (event) {
+// showHideTwo(event);
+//});
+
+console.log(data);
+const ul = document.getElementById("my-list");
+data.forEach((id) => {
+  const li = document.createElement("li");
+  li.innerHTML = id.name;
+  ul.appendChild(li);
 });
