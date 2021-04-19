66b044ff32f5157115de70cc6087ae8d6a8403ed
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-19 16:52:23 +0200

    3.3 commit

--- table.js
+++ table.js
@@ -53,47 +53,6 @@ fetch("https://api.thedogapi.com/v1/breeds?x-api-key=41fbcc65-cb08-4b79-91e8-c49
     //    tr.appendChild(td);
   //  });
 //}
-//const displayDataTree = (data) => {
- //  const tr = document.getElementById("group");
-   // data.forEach((post) => {
-   //     const td = document.createElement("td");
-    //    td.innerHTML = post.breed_group;
-     //   tr.appendChild(td);
-    //});
-//}
-//const displayDataFour = (data) => {
-   //const tr = document.getElementById("span");
-   // data.forEach((post) => {
-   //     const td = document.createElement("td");
-   //     td.innerHTML = post.life_span;
-    //    tr.appendChild(td);
-   // });
-//}
-
-    //data.forEach((row) => {
-      //  const tr= document.createElement("tr");
-        //row.forEach((cell) => {
-          //  const td = document.createElement("td");
-           // td.textContent = cell;
-          //  tr.appendChild(td);
-       // })
-       // tableBody.appendChild(tr); 
-   // })
-//}
-
-//const addData = (data) => {
-  //  const tr = document.getElementById("span");
-   // data.forEach((post) => {
-    //    var newRow = document.createElement("tr");
-    //    var newCell = document.createElement("td");
-     //   newCell.innerHTML = post.life_span;
-     //   newRow.append(newCell);
-     //   document.getElementById("span").appendChild(newRow);
-      //  document.getElementById("span").value = '';
-  // });
-//}
-  
-
 
     
     
