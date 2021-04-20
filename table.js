56ca59ff3f012f3903cff005968eb0a8aa61ad74
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-20 17:43:26 +0200

    4th commit

--- table.js
+++ table.js
@@ -18,41 +18,19 @@ fetch("https://api.thedogapi.com/v1/breeds?x-api-key=41fbcc65-cb08-4b79-91e8-c49
     }
       }
 )
-        //console.log(data);
         //displayData(data);
         //displayDataTwo(data);
         //displayDataTree(data);
         //displayDataFour(data);
         //addData(data);
         //addDataOne(data);
-
-
     .catch((error) => {
         console.log(error);
     })
 
 
-//const displayData = (data) => {
-  //  const ul = document.getElementById("my-list");
-  //  data.forEach((post) => {
-  //      const li = document.createElement("li");
-  //      li.innerHTML = post.name;
-  //      ul.appendChild(li);
-  //  });
-//};
-
-//const tableBody = document.querySelector("#newTable > tbody");
-//console.log(tableBody);
 
 
-//const displayDataTwo = (data) => {
- //  const tr = document.getElementById("newT");
-  //  data.forEach((post) => {
-   //     const td = document.createElement("td");
-   //     td.innerHTML = post.name;
-    //    tr.appendChild(td);
-  //  });
-//}
 
 const checkboxElems = document.querySelectorAll("input[type='checkbox']");
 checkboxElems.addEventListener("change", function() {
@@ -62,8 +40,4 @@ checkboxElems.addEventListener("change", function() {
         // Checkbox is not checked..
     }
 });
-console.log(checkboxElems);
 
-//const checkboxElems = document.querySelectorAll("input[type='checkbox']").forEach((checkboxElems) => {
- // checkboxElems.addEventListener("change", function ())
-//});
