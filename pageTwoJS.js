9841fe38ce3a0551b6f8192eacc1121ec9cfada3
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-25 15:35:52 +0200

    5th commit

---
+++ pageTwoJS.js
@@ -0,0 +1,34 @@
+fetch("https://api.thedogapi.com/v1/breeds/search?x-api-key=41fbcc65-cb08-4b79-91e8-c497c6dba122&q=terrier")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    }) 
+    .then((data) => {
+        console.log(data)
+        displayTerrier(data)
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+const displayTerrier = (data) => {
+   data.forEach (post => {
+       let card = document.createElement("div");
+       card.classList.add("card");
+
+       let name = document.createElement("h5");
+       name.innerHTML = post.name;
+       card.appendChild(name);
+       
+       let description = document.createElement("p")
+       description.innerHTML = post.temperament;
+       card.appendChild(description);
+       
+       let bredFor = document.createElement("p")
+       bredFor.innerHTML = post.bred_for
+       card.appendChild(bredFor);
+
+       let container = document.querySelector("#container");
+       container.appendChild(card);
+   });
+}
