1005f5a2778abdb51c22dd193e0ddbac5c25b353
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-29 12:40:53 +0200

    8.1 commit

--- index.js
+++ index.js
@@ -30,34 +30,6 @@ const showHide =(event) => {
        less.style.display = 'none';
     }
 }
-//function showHide(event) {
-  //  console.log(event.target.closest('div').id)
-   // const num =event.target.closest('div').id
-  //  const dots = document.getElementById(num+"-dots");
-  //  const less = document.getElementById(num + "-less");
-  //  const more = document.getElementById(num + "-more");
-  //  if (dots.style.display !== 'none') {
-  //      dots.style.display = 'none';
-  //      more.style.display = 'inline';
-   //     less.style.display = 'inline';
-   // } else {
-   //     dots.style.display = 'inline';
-   //     more.style.display = 'none';
-   //     less.style.display = 'none';
-   // }
-//}
-
-//function showHideTwo(event) {
- // let moreText = document.getElementById("moreText");
- // let buttonID = event.target.id;
- // if (!moreText.classList.contains("show")) {
-//    moreText.classList.add("show");
- //   document.getElementById(buttonID).innerHTML = "Show Less";
-//  } else {
- //   moreText.classList.remove("show");
-//    document.getElementById(buttonID).innerHTML = "Show More";
- // }
-//}
 
 const showHideTwo = (event) => {
   let moreText = document.getElementById("moreText");
